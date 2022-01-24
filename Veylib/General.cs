30e5c603fe1d78e2c7b580f66f42d3867de7c860
verlox <verlox@riseup.net>
2022-01-23 22:45:53 -0500

    add: workload distributer / better thread work distro

--- Veylib/General.cs
+++ Veylib/General.cs
@@ -1,7 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Veylib
 {
+    public static class WorkLoadController
+    {
+        public static List<List<T>> Seperate<T>(List<T> items, int loadCount)
+        {
+            var loads = new List<List<T>>();
+
+            // Create lists
+            for (int x = 0; x < loadCount; x++)
+                loads.Add(new List<T>());
+
+            // Actually seperate the loads
+            for (int x = 0; x < items.Count; x++)
+                loads[x % loadCount].Add(items[x]);
+
+            // Remove blank loads
+            loads = loads.Where(load => load.Count > 0).ToList();
+
+            return loads;
+        }
+    }
+
     public class General
     {
         /// <summary>
@@ -26,7 +49,6 @@ namespace Veylib
         {
             return ((DateTimeOffset)DateTime.SpecifyKind(from, DateTimeKind.Utc)).ToUnixTimeSeconds();
         }
-
 
         /// <summary>
         /// Get a DateTime from epoch time
