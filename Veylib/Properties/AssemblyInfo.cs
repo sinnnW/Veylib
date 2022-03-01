cd545c0a71fcad26a774b8fa60e6494db3e6f5b3
verlox <verlox@riseup.net>
2022-02-28 19:45:44 -0500

    update assembly info

--- Veylib/Properties/AssemblyInfo.cs
+++ Veylib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Veylib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("General purpose library for .NET framework applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Veylib")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © verlox 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
